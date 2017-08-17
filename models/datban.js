62aa8fd1eb7293a1044429c00f957d84685249de
lephuongtu271094 <lephuongtu271094@gmail.com>
2017-08-17 17:40:29 +0700

    update

---
+++ models/datban.js
@@ -0,0 +1,16 @@
+const {db, } = require('../pgp');
+
+class DatCho{
+    constructor(db){
+        this.db = db
+    }
+
+    insertDatban(datban){
+        return this.db.one('INSERT INTO datban(time_dat,date_dat,soluong,name_dat,so_dat,email_dat,ghichu)' +
+            ' VALUES(${date}, ${time}, ${SoLuong},${Name_DatBan},${Number_DatBan},${Email_DatBan},${Ghi_Chu}) RETURNING *', datban)
+    }
+
+}
+
+
+module.exports = new DatCho(db);
--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,53 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const DatBan = require('../models/datban')
+
 
 /* GET home page. */
+//-------------- RENDER TRANG CHỦ-------------------
 router.get('/', function(req, res, next) {
   res.render('trangchu.html', { title: 'Home' });
 });
+
+// ------------- RENDER CHI TIẾT SẢN PHẨM----------------
+
 router.get('/chitiet', function(req, res, next) {
   res.render('chitiet.html', { title: 'Chi Tiết' });
+});
+router.post('/datban', function(req, res, next) {
+
+    req.checkBody('SoLuong' , 'Họ và Tên không được để trống').notEmpty();
+    req.checkBody('Name_DatBan' , 'Họ và Tên không được để trống').notEmpty();
+    req.checkBody('Number_DatBan' , 'Số điênh thoại không được để trống').notEmpty();
+    req.checkBody('Email_DatBan' , 'Email không được để trống').notEmpty();
+    req.checkBody('Email_DatBan' , 'Không phải Email').isEmail();
+
+    let errors = req.validationErrors();
+    if(errors){
+        res.json({errors: errors})
+    }else {
+        DatBan.insertDatban(req.body)
+            .then(data => {
+                console.log(data)
+            })
+    }
+
+});
+
+
+//----------------RENDER DANH SÁCH SẢN PHẨM----------------
+
+router.get('/danhsach', function(req, res, next) {
+  res.render('danhsach.html', { title: 'Danh sách' });
 });
 
 
 
+//-------------------------------------------ADMIN-------------------------------------------
 
 router.get('/admin', function(req, res, next) {
   res.render('admin.html', { title: 'New book' });
 });
-
-
 router.get('/admin/posts_list', function(req, res, next) {
   res.render('posts_list.html', { title: 'Posts List' });
 });
@@ -34,8 +65,6 @@ router.get('/admin/posts_categories', function(req, res, next) {
 router.get('/admin/login', function(req, res, next) {
   res.render('login.html', { title: 'Login' });
 });
-
-
 router.get('/admin/new_user', function(req, res, next) {
   res.render('create_user.html', { title: 'New User Admin' });
 });
@@ -43,4 +72,6 @@ router.get('/admin/new_user', function(req, res, next) {
 router.get('/admin/posts', function(req, res, next) {
   res.render('posts.html', { title: 'Thêm Mới' });
 });
+
+
 module.exports = router;
